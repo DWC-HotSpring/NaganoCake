a5561ce4becaae15721332df793e8d765b3b2dba
vagrant <vagrant@localhost.localdomain>
2020-08-16 05:52:14 +0000

    [Add]ジャンル機能追加

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,4 @@
 class Genre < ApplicationRecord
+    validates :is_active,  inclusion: { in: [true, false]}
+    #enum is_active: {active: true, inactive: false}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2020_08_15_092459) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.boolean "is_valied", default: true, null: false
+    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
